fab2c2dc78e7f8fb25d30f12287216b2a33cbcab
Rodrigo Freitas <rodrigo.d.freitas@outlook.com>
2025-05-15 20:12:04 -0300

    Página "Sobre mim"
    Criada a estrutura da página

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
+import { AboutMeComponent } from './pages/about-me/about-me.component';
 
 export const routes: Routes = [
     { path: '', component: HomeComponent },
+    { path: 'sobre-mim', component: AboutMeComponent }
 ];
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,17 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
+import { HomeComponent } from './app/pages/home/home.component';
+import { AboutMeComponent } from './app/pages/about-me/about-me.component';
+import { provideRouter } from '@angular/router';
 
-bootstrapApplication(AppComponent, appConfig)
-  .catch((err) => console.error(err));
+const routes = [
+  { path: '', component: HomeComponent }, // Rota inicial
+  { path: 'sobre-mim', component: AboutMeComponent } // Rota para "About"
+];
+
+bootstrapApplication(AppComponent, {
+  providers: [
+    provideRouter(routes)
+  ]
+}).catch(err => console.error(err));
