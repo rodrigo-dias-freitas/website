e082ffabe4d7cab6041f8f3aa173e81745717fd4
Rodrigo Freitas <rodrigo.d.freitas@outlook.com>
2025-05-16 13:43:04 -0300

    Feature: Button component

---
+++ src/app/components/button/button.component.ts
@@ -0,0 +1,25 @@
+import { CommonModule } from '@angular/common';
+import { Component, EventEmitter, Input } from '@angular/core';
+import { RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'app-button',
+  standalone: true,
+  imports: [CommonModule, RouterLink],
+  templateUrl: './button.component.html',
+  styleUrl: './button.component.scss'
+})
+export class ButtonComponent {
+
+  @Input() label: string = 'Enviar';
+  @Input() type: 'cancel' | 'save' | 'delete' | 'custom' = 'custom';
+  @Input() icon?: string;
+  @Input() link?: string;
+  @Input() action = new EventEmitter<void>();
+
+  onClick(){
+    this.action.emit();
+  }
+  
+  
+}
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,12 +1,16 @@
 import { Component } from '@angular/core';
+import { ButtonComponent } from '../../components/button/button.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [ButtonComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
 export class HomeComponent {
 
+  onClickGoToAboutMe(){
+    console.log('Link');
+  }
 }
