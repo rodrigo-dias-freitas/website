4b28cc2e27a356ad5577fd45437a8174e1be429f
Rodrigo Freitas <rodrigo.d.freitas@outlook.com>
2025-07-29 22:00:29 -0300

    style: aplicar estilo para responsividade
    Responsividade para página de contatos

--- src/app/components/form/form.component.ts
+++ src/app/components/form/form.component.ts
@@ -1,15 +1,19 @@
 import { Component } from '@angular/core';
 import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-form',
   standalone: true,
-  imports: [ReactiveFormsModule, HttpClientModule],
+  imports: [ReactiveFormsModule, HttpClientModule, CommonModule],
   templateUrl: './form.component.html',
   styleUrl: './form.component.scss'
 })
 export class FormComponent {
+
+  successMessage = '';
+  submitted = false;
 
   contactForm = new FormGroup({
     name: new FormControl('', Validators.required),
@@ -22,12 +26,22 @@ export class FormComponent {
   constructor(private http: HttpClient) {}
 
   onSubmit() {
+
+    this.submitted = true;
+
     if(this.contactForm.valid){
       const formData = this.contactForm.value;
 
       this.http.post('https://rodrigofreitas.dev.br/wordpress/wp-json/custom/v1/mensagem', formData).subscribe({
         next: res =>{
+          this.successMessage = 'Mensagem enviada com sucesso!';
           this.contactForm.reset();
+          this.submitted = false;
+
+          //Oculta a mensagem após 5s
+          setTimeout(() => {
+            this.successMessage = '';
+          }, 5000);
         },
         error: err => {
           
